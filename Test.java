e4cacbb9cc6e4baa0820b82e2ab376be8627285a
Eda Nur Yardım <94635163+edanuryardim@users.noreply.github.com>
2024-04-09 17:28:12 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+package GeneticAlgorithms;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Solution[] sList = new Solution[10];
+
+        for(int i=0;i<10;i++) {
+            Solution s= new Solution(10);
+            sList[i]=s;
+            System.out.println(s.toString() + " Fitness = " + s.FitnessValue());
+        }
+        System.out.println("--------------------");
+        for(int i=0;i<10;i=i+2) {
+            Solution offspring = sList[i].Crossover(sList[i+1]);
+            System.out.println(offspring.toString() +
+                    " Fitness = " + offspring.FitnessValue());
+        }
+
+    }
+}
